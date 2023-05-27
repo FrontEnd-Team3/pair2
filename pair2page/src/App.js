91f9c44ed0282032b1d6026a3638de00824b9113
jeongwooHam <hamjw0122@gmail.com>
2023-05-27 22:15:43 +0900

    navbar 상세 구현 완료

--- pair2page/src/App.js
+++ pair2page/src/App.js
@@ -3,13 +3,16 @@ import React from 'react'
 import 'bootstrap/dist/css/bootstrap.css'
 import router from './routes/routing'
 import { RouterProvider } from 'react-router-dom'
+import NavStoreProvider from './context/navcontent'
 
 function App() {
 	const routing = router
 	return (
-		<div className="Apps">
-			<RouterProvider router={router} />
-		</div>
+		<NavStoreProvider>
+			<div className="Apps">
+				<RouterProvider router={router} />
+			</div>
+		</NavStoreProvider>
 	)
 }
 
