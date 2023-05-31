1163ea070820ebfce78619d3cf51976ef00b8632
zivivle <zivivle@gmail.com>
2023-06-01 00:01:42 +0900

    메인페이지 이미지 캐러셀까지 구현 완료

--- pair2page/src/components/carousel/index.js
+++ pair2page/src/components/carousel/index.js
@@ -1,43 +1,83 @@
-import 'slick-carousel/slick/slick.css'
-import 'slick-carousel/slick/slick-theme.css'
+import { useEffect, useState } from 'react'
+import { Button } from 'reactstrap'
+import styled from 'styled-components'
 
 const CarouselBox = () => {
-	const setting = {
-		slide: 'div', //슬라이드 되어야 할 태그 ex) div, li
-		infinite: true, //무한 반복 옵션
-		slidesToShow: 4, // 한 화면에 보여질 컨텐츠 개수
-		slidesToScroll: 1, //스크롤 한번에 움직일 컨텐츠 개수
-		speed: 100, // 다음 버튼 누르고 다음 화면 뜨는데까지 걸리는 시간(ms)
-		arrows: true, // 옆으로 이동하는 화살표 표시 여부
-		dots: true, // 스크롤바 아래 점으로 페이지네이션 여부
-		autoplay: true, // 자동 스크롤 사용 여부
-		autoplaySpeed: 10000, // 자동 스크롤 시 다음으로 넘어가는데 걸리는 시간 (ms)
-		pauseOnHover: true, // 슬라이드 이동	시 마우스 호버하면 슬라이더 멈추게 설정
-		vertical: false, // 세로 방향 슬라이드 옵션
-		prevArrow: "<button type='button' class='slick-prev'>Previous</button>", // 이전 화살표 모양 설정
-		nextArrow: "<button type='button' class='slick-next'>Next</button>", // 다음 화살표 모양 설정
-		dotsClass: 'slick-dots', //아래 나오는 페이지네이션(점) css class 지정
-		draggable: true, //드래그 가능 여부
+	const [imgState, setImgState] = useState([])
 
-		responsive: [
-			// 반응형 웹 구현 옵션
-			{
-				breakpoint: 960, //화면 사이즈 960px일 때
-				settings: {
-					//위에 옵션이 디폴트 , 여기에 추가하면 그걸로 변경
-					slidesToShow: 3,
-				},
-			},
-			{
-				breakpoint: 768, //화면 사이즈 768px일 때
-				settings: {
-					//위에 옵션이 디폴트 , 여기에 추가하면 그걸로 변경
-					slidesToShow: 2,
-				},
-			},
-		],
+	const [currentIndex, setCurrentIndex] = useState(0)
+
+	useEffect(() => {
+		imgList()
+	}, [])
+
+	const imgList = () => {
+		const images = []
+		for (let i = 1; i <= 15; i++) {
+			images.push(`/img/makeup-product-img/makeup${i}.png`)
+		}
+		setImgState(images)
 	}
-	return
+
+	const handleCarousel = direction => {
+		if (direction === 'next') {
+			setCurrentIndex(prevIndex =>
+				prevIndex === imgState.length - 1 ? 0 : prevIndex + 1,
+			)
+		} else if (direction === 'prev') {
+			setCurrentIndex(prevIndex =>
+				prevIndex === 0 ? imgState.length - 1 : prevIndex - 1,
+			)
+		}
+	}
+
+	return (
+		<CarouselContainer>
+			<CarouselList
+				style={{ transform: `translateX(-${currentIndex * 24}vw)` }}
+			>
+				{imgState.map((img, index) => (
+					<div>
+						<CaBox key={index}>
+							<img src={img} />
+						</CaBox>
+					</div>
+				))}
+			</CarouselList>
+			<ButtonBox>
+				<Button onClick={() => handleCarousel('prev')}>•</Button>
+				<Button onClick={() => handleCarousel('next')}>•</Button>
+			</ButtonBox>
+		</CarouselContainer>
+	)
 }
 
 export default CarouselBox
+
+const CarouselContainer = styled.div`
+	overflow: hidden;
+`
+
+const CaBox = styled.div`
+	margin: 0;
+	width: fit-content;
+
+	img {
+		width: 24vw;
+		float: left;
+		margin-left: 10px;
+	}
+`
+const ButtonBox = styled.div`
+	margin-left: 45vw;
+	margin-bottom: 20px;
+	margin-top: 30px;
+	Button {
+		margin-left: 20px;
+	}
+`
+
+const CarouselList = styled.div`
+	display: flex;
+	transition: transform 0.3s ease;
+`
